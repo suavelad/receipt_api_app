b0cef68c541ba7052ccdb30700fcb3c041a5ef03
Sunday Ajayi <sunnexajayi@gmail.com>
2021-07-14 09:20:39 +0100

    project update

--- receipts/urls.py
+++ receipts/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from rest_framework.documentation import include_docs_urls
 from django.views.generic import RedirectView
 from django.contrib.staticfiles.views import serve
