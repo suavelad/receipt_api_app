b115085f7d16fe2386d29bd93813728dbc909a13
Sunday Ajayi <sunnexajayi@gmail.com>
2021-07-14 23:37:18 +0100

    update the git ignore file

--- receipt_app/apis.py
+++ receipt_app/apis.py
@@ -1,22 +1,15 @@
-from .serializers import ReceiptSerializer, CreateListMixin, ReceiptsSerializer,UserSerializer
+from .serializers import UserSerializer
 from .schemas import ReceiptSchema
 from .models import Receipts
-from rest_framework.viewsets import ModelViewSet, GenericViewSet
-from rest_framework.response import Response
 from django.db import connection
 import collections
-from rest_framework import status, views, permissions,generics
-from rest_framework import serializers
+from rest_framework import  permissions,generics
 from django.core.exceptions import ValidationError
-
-from rest_framework.permissions import AllowAny
-from rest_framework.generics import CreateAPIView, ListCreateAPIView
-from rest_framework.mixins import CreateModelMixin
 from rest_framework_bulk import  ListBulkCreateUpdateDestroyAPIView
 
 
-
 class ReceiptView(ListBulkCreateUpdateDestroyAPIView):
+
     schema =ReceiptSchema
     queryset = Receipts.objects.all()
     serializer_class = UserSerializer
--- receipt_app/urls.py
+++ receipt_app/urls.py
@@ -3,17 +3,11 @@ from rest_framework.routers import DefaultRouter
 from .apis import  ReceiptView
 
 router = DefaultRouter()
-# router.register('create/receipt',CreateReceiptView)
 urlpatterns = router.urls
 
 urlpatterns += [
-    # path('generate/receipt/', GenReceiptsView.as_view(), name='generate_receipt'),
-    # path('create/receipt/',CreateReceiptView.as_view())
+
     path('create/receipt/',ReceiptView.as_view())
-
-
-    
-
 
 
 ]
